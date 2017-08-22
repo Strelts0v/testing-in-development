61a6d2e6b67e285b6bf49ab86bd7eabfe71db8a6
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-22 14:45:30 +0300

    update interface HeroService

--- server/src/main/java/com/vg/server/service/HeroService.java
+++ server/src/main/java/com/vg/server/service/HeroService.java
@@ -3,14 +3,22 @@ package com.vg.server.service;
 import com.vg.server.model.Hero;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface HeroService {
 
     Hero findHeroById(long id);
 
-    Iterable<Hero> findHeroesByName(String namePattern);
+    List<Hero> findHeroesByName(String namePattern);
 
-    Iterable<Hero> getHeroes(int count);
+    List<Hero> getHeroesLimited(int count);
 
     Iterable<Hero> getAllHeroes();
+
+    Hero addHero(Hero hero);
+
+    int updateHero(long id, Hero hero);
+
+    void deleteHero(long id);
 }
